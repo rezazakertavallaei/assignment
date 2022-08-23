463d57ed396bb6313441313315a69fd795523ff7
reza zaker tavallaei <110588050+rezazakertavallaei@users.noreply.github.com>
2022-08-23 13:31:58 +0430

    Update tass.py

--- assignment 3/tass.py
+++ assignment 3/tass.py
@@ -9,8 +9,9 @@ while True:
     print(x)
     sum+=x
     count+=1
-    if x==6:
-
-        break 
+    if x!=6:
+        break
+    else:
+        continue 
 print(sum)
 print("tedad tekrar rikhtan tass:  ",count)
